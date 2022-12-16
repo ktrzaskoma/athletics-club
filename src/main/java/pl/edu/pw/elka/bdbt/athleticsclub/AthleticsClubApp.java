787878c316fc692fae081d293b49babe6889ae47
ktrzaskoma <kttrzaskoma@gmail.com>
2022-12-16 17:52:51 +0100

    init

---
+++ src/main/java/pl/edu/pw/elka/bdbt/athleticsclub/AthleticsClubApp.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.elka.bdbt.athleticsclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthleticsClubApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthleticsClubApp.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pw/elka/bdbt/athleticsclub/AthleticsClubAppTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.elka.bdbt.athleticsclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthleticsClubAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
